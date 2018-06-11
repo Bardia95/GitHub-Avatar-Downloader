b0cf39741d6cc9828be24a4923bb2f6fc7c609f7
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-06-11 13:04:13 -0400

    downloading

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -22,11 +23,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+function downloadImagebyURL(url, filePath) {
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ' + response.statusCode + '\n' + response.headers['content-type']);
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   var obj = JSON.parse(result);
   for (var i = 0; i < obj.length; i++) {
-    console.log(obj[i]['avatar_url']);
+    downloadImagebyURL(obj[i]['avatar_url'], `./avatars/${obj[i]['login']}.jpg`);
   }
-  // }
 });
