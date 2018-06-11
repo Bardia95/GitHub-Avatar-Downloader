b5953a29e40520e13f734decf1a3db21d70e0b28
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-06-11 12:24:08 -0400

    git ignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,21 @@
 var request = require('request');
 
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+var token = require('./secrets.js')
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': token
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
